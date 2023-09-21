8abe6e43140b51b6f7e25cbf3dc80b3cf661cb26
T0-x-1c <144830006+T0-x-1c@users.noreply.github.com>
2023-09-21 18:25:17 +0300

    Add files via upload

--- logika/m2/u1/students.py
+++ logika/m2/u1/students.py
@@ -1,11 +1,16 @@
+import time
+
 class Student():
     def __init__(self, name, sername, mark):
         self.name = name
         self.sername = sername
         self.mark = mark
 
+start_time = time.time()
 
 students = []
+
+all_mark = []
 
 with open('studens.txt', encoding='utf-8') as file:
     for line in file:
@@ -13,6 +18,47 @@ with open('studens.txt', encoding='utf-8') as file:
         obj = Student(data[0], data[1], int(data[2]))
         students.append(obj)
 
-for f in students:
-    if f.mark == 5:
-        print(f.name, f.sername, f.mark)
+for student in students:
+    if student.mark == 5:
+        print(student.name, student.sername, student.mark)
+
+
+all_mark.append(student.mark)
+GPA = sum(all_mark) / len(all_mark)
+print(GPA)
+
+finish_time = time.time()
+
+elapsed_time = finish_time - start_time
+
+print(f'Часу потрібно для читання файлу: {elapsed_time:.5f}')
+
+print('-----------------------')
+
+start_time = time.time()
+
+students = []
+
+all_mark = []
+
+with open('students_large.txt', encoding='utf-8') as file:
+    for line in file:
+        data = line.split(' ')
+        obj = Student(data[0], data[1], int(data[2]))
+        students.append(obj)
+
+for student in students:
+    if student.mark == 5:
+        print(student.name, student.sername, student.mark)
+
+
+all_mark.append(student.mark)
+GPA = sum(all_mark) / len(all_mark)
+print(GPA)
+
+finish_time = time.time()
+
+elapsed_time = finish_time - start_time
+
+print(f'Часу потрібно для читання файлу: {elapsed_time:.5f} cек')
+print('тут іде більше часу тому що , наш код прочитує кожну строку з/n студентами а їх тут більше , тому і наш код просчитує файл довше')
---
+++ logika/m2/u1/students_large.py
@@ -0,0 +1,35 @@
+import time
+
+class Student():
+    def __init__(self, name, sername, mark):
+        self.name = name
+        self.sername = sername
+        self.mark = mark
+
+start_time = time.time()
+
+students = []
+
+all_mark = []
+
+with open('students_large.txt', encoding='utf-8') as file:
+    for line in file:
+        data = line.split(' ')
+        obj = Student(data[0], data[1], int(data[2]))
+        students.append(obj)
+
+for student in students:
+    if student.mark == 5:
+        print(student.name, student.sername, student.mark)
+
+
+all_mark.append(student.mark)
+GPA = sum(all_mark) / len(all_mark)
+print(GPA)
+
+finish_time = time.time()
+
+elapsed_time = finish_time - start_time
+
+print(f'Часу потрібно для читання файлу: {elapsed_time:.5f} cек') # тут іде більше часу тому що , наш код прочитує кожну строку з -
+#студентами а їх тут більше , тому і наш код просчитує файл довше

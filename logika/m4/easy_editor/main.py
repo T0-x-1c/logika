02cf41bfc417c34068e2dd497ebf325d4a9c3ec5
T0-x-1c <144830006+T0-x-1c@users.noreply.github.com>
2023-10-27 20:01:35 +0300

    Add files via upload

--- logika/m4/easy_editor/main.py
+++ logika/m4/easy_editor/main.py
@@ -8,7 +8,7 @@ from PyQt5.QtWidgets import (
     QPushButton, QListWidget, QHBoxLayout, QVBoxLayout
 )
 
-# from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter
 # from PIL.ImageQt import ImageQt  # Для перенесення графіки з Pillow до QT
 # from PIL.ImageFilter import SHARPEN
 
@@ -45,9 +45,15 @@ all_btn = [btn_pict_left, btn_pict_right, btn_mirror, btn_sharpness, btn_bw]
 
 for btn in all_btn:
     btn.setStyleSheet('''
-    background-color: rgb(214,255,255);
-    border: 3px solid rgb(153,255,255);
-    border-radius:4px;
+    QPushButton {
+        background-color: rgb(214,255,255);
+        border: 3px solid rgb(153,255,255);
+        border-radius:4px;
+        }
+    
+    QPushButton:hover {
+            color: yellow;
+        }
                         ''')
 
 col1.addWidget(btn_folder)
@@ -65,11 +71,6 @@ col3.addLayout(col2)
 osn_layout.addLayout(col1, stretch=2)
 osn_layout.addLayout(col3, stretch=6)
 
-
-workdir = QFileDialog.getExistingDirectory()
-file_and_folders = os.listdir(workdir)
-print(file_and_folders)
-
 def filter(files):
     result = []
     ext = ['jpg', 'jpeg', 'bmp', 'gif', 'jfif', 'svg', 'png']
@@ -80,8 +81,50 @@ def filter(files):
 
     return result
 
-lst_folder.addItems(filter(file_and_folders))
+def show_file():
+    global workdir
+    workdir = QFileDialog.getExistingDirectory()
+    #c:\sistr
+    file_and_folders = os.listdir(workdir)
+    #["gomer1.jpg", "gomer2.jpg"]
+    filtered_img = (filter(file_and_folders))
 
+    lst_folder.clear()
+    lst_folder.addItems(filtered_img)
+
+class ImageProcesor():
+    def __init__(self):
+        self.filename = None
+        self.original = None
+        self.save_dir = 'Modifiet/'
+
+    def loadimage(self, fielname):
+        self.filename = fielname
+
+        full_path = os.path.join(workdir, fielname)
+        self.original = Image.open(full_path)
+
+    def show_image(self, path):
+        lb_pict.hide()
+        pixmapeimage = QPixmap(path)
+
+        w,h = lb_pict.width(), lb_pict.height()
+
+        pixmapeimage = pixmapeimage.scaled(w,h, Qt.KeepAspectRatio)
+
+        lb_pict.setPixmap(pixmapeimage)
+        lb_pict.show()
+
+def showChosenImage():
+    file_name = lst_folder.currentItem().text()
+    work_img.loadimage(file_name)
+    full_path = os.path.join(workdir, file_name)
+    work_img.show_image(full_path)
+
+work_img = ImageProcesor()
+
+btn_folder.clicked.connect(show_file)
+lst_folder.itemClicked.connect(showChosenImage)
 
 window.setLayout(osn_layout)
 window.show()

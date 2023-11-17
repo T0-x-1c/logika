bb4d768c0ec86bad6f2d89a9e2c88647174de41e
T0-x-1c <144830006+T0-x-1c@users.noreply.github.com>
2023-11-17 20:04:43 +0200

    Add files via upload

---
+++ logika/m5/maze/maze.py
@@ -0,0 +1,52 @@
+#створи гру "Лабіринт"!
+import time
+
+from pygame import *
+from pygame.sprite import collide_rect
+from pygame.transform import scale, flip
+from pygame.image import load
+from random import randint
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = scale(load(player_image), (70,70))
+        self.speed = player_speed
+
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+win_w = 700
+win_h = 500
+window = display.set_mode((win_w, win_h))
+
+background = scale(image.load("background.jpg"), (win_w, win_h))
+
+player = GameSprite('hero.png', 5, win_h - 70, 4)
+
+cyborg = GameSprite('cyborg.png', win_w - 100, win_h - 300, 2)
+
+clock = time.Clock()
+FPS = 60
+game = True
+
+mixer.init()
+mixer.music.load("jungles.ogg")
+mixer.music.play(-1)
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    window.blit(background, (0,0))
+
+    player.reset()
+    cyborg.reset()
+
+    display.update()
+    clock.tick(FPS)
